c2843094b25c0483f85f9e60a5a306bfc7a15fce
uhlyarikp <peter.uhlyarik@gmail.com>
2020-10-07 17:23:18 +0200

    clear

--- view/footer.php
+++ view/footer.php
@@ -13,11 +13,6 @@
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="js/jquery-validation-1.19.1/dist/jquery.validate.min.js"></script>
-<script src="js/jquery-validation-1.19.1/dist/additional-methods.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.js"></script>
 <script src="js/functions.js"></script>
-<script src="js/cookiealert.js"></script>
--- view/header.php
+++ view/header.php
@@ -15,6 +15,4 @@
 <meta http-equiv="Content-Language" content="<?=pageLang?>">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="css/custom.css">
-<link rel="stylesheet" href="css/cookiealert.css">
